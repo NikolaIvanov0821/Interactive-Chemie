85859b42d02edd46b0284233659836ccde84128c
NikolaIvanov0821 <ni.ivanov2008@gmail.com>
2024-02-25 23:18:28 +0200

    Initial commit

--- script/reactions.js
+++ script/reactions.js
@@ -1,30 +1,36 @@
-const root = document.getElementById("choose-elements")
+import { html, render } from "../node_modules/lit-html/lit-html.js";
 
+const filter = document.getElementById("filter")
+const articles = document.getElementById("articles")
 
 const response = await fetch("../jsons/elements.json")
 const elements = await response.json()
+console.log(elements);
 
-for (const element in elements.elements) {
-    const symbol = elements.elements[element].symbol
-    const type = elements.elements[element].type
-    const elBtn = document.createElement("button")
-    elBtn.textContent = symbol
-    elBtn.className = type
-    root.appendChild(elBtn)
+// const articleTemp = (equation, description, fact) => html`<article id="article">
+//                             <h2>${equation}</h2>
+//                             <ol>
+//                                 <li>${description}</li>
+//                                 <li>${fact}</li>
+//                             </ol>
+//                         </article>`
 
-    elBtn.onclick = click({element, symbol, type})
-}
-
-function click(info) {
+let count = 0
+for (const reaction in elements) {
+    count++
     
+    const equation = reaction
+    const description = elements[reaction].description
+    const fact = elements[reaction]["interesting_fact"]
+    //render(articleTemp(equation, description, fact), articles)
+    const div = document.createElement("div")
+    div.innerHTML = `<article id="article">
+    <h2>${equation}</h2>
+    <ol>
+        <li>${description}</li>
+        <li>${fact}</li>
+    </ol>
+</article>`
+    articles.appendChild(div)
 }
-function reaction(type) {
-    switch (type) {
-        case value:
-            
-            break;
-    
-        default:
-            break;
-    }
-}
+
