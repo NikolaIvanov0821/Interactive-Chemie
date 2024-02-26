ae0faf32692128a8d3efb22e095adc69237acc01
NikolaIvanov0821 <ni.ivanov2008@gmail.com>
2024-02-26 22:08:20 +0200

    Initial commit

--- script/reactions.js
+++ script/reactions.js
@@ -1,5 +1,7 @@
 const filter = document.getElementById("filter")
-const articles = document.getElementById("articles")
+const articles = document.getElementById("articles-1")
+const articles2 = document.getElementById("articles-2")
+const articles3 = document.getElementById("articles-3")
 
 const response = await fetch("../jsons/elements.json")
 const elements = await response.json()
@@ -20,15 +22,41 @@ for (const reaction in elements) {
     const equation = reaction
     const description = elements[reaction].description
     const fact = elements[reaction]["interesting_fact"]
-    //render(articleTemp(equation, description, fact), articles)
     const div = document.createElement("div")
     div.innerHTML = `<article id="article">
     <h2>${equation}</h2>
-    <ol>
+    <ul>
         <li>${description}</li>
         <li>${fact}</li>
-    </ol>
+    </ul>
 </article>`
-    articles.appendChild(div)
+    if (count <= 5) {
+        articles.appendChild(div)
+    } else if (count > 5 && count <= 10) {
+        articles2.appendChild(div)
+    } else if (count > 10 && count <= 15) {
+        articles3.appendChild(div)
+    }
+    
 }
 
+const one = document.getElementById("to-first")
+one.onclick = () => {
+    articles.style.display = 'flex'
+    articles2.style.display = 'none'
+    articles3.style.display = 'none'
+}
+
+const two = document.getElementById("to-second")
+two.onclick = () => {
+    articles.style.display = 'none'
+    articles2.style.display = 'flex'
+    articles3.style.display = 'none'
+}
+
+const three = document.getElementById("to-third")
+three.onclick = () => {
+    articles.style.display = 'none'
+    articles2.style.display = 'none'
+    articles3.style.display = 'flex'
+}
